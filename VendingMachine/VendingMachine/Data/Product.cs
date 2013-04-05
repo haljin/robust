bd7a0d35967da60762c2f2b08f6c2436d7170936
Pawel Antemijczuk <paul.shinobi@gmail.com>
2013-04-05 13:46:42 +0200

    first tests

---
+++ VendingMachine/VendingMachine/Data/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace VendingMachine.Data
+{
+    class Product
+    {
+        String Name { get; set; }
+        int Ammount { get; set; }
+        decimal Price { get; set; }
+        
+        public Product(String name, int amm, decimal price)
+        {
+            Name = name;
+            Ammount = amm;
+            Price = price;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(Ammount >= 0);
+            Contract.Invariant(Price >= 0);
+        }
+
+    }
+
+
+}
